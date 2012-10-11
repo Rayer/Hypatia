60068bd53d78ae35f26460c701eace15239fc486
Rayer <rayer@vista.aero>
2012-10-11 12:04:50 +0800

    Initial Commit

---
+++ MinServer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MinServer
+//
+//  Created by Rayer Shih on 10/11/12.
+//  Copyright (c) 2012 Digital Resolve. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
